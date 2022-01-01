2b8fb31eb09b2c4b6680aa94123a448bb3d2ec89
Eduardo S. Libardi <eslibardi@gmail.com>
2022-01-01 20:54:05 -0300

    initial setup for netlify

---
+++ src/pages/index.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import {
+  Layout,
+  Hero,
+  About,
+  Survey,
+  Slider,
+  GridProjects,
+  Seo,
+} from '../components'
+
+const HomePage = ({ data }) => {
+  const {
+    allAirtable: { nodes: projects },
+    customers: { nodes },
+  } = data
+  return (
+    <>
+      <Seo title="Home" />
+      <Layout>
+        <Hero />
+        <About />
+        <GridProjects projects={projects} title="latest projects" />
+        <Survey />
+        <Slider customers={nodes} />
+      </Layout>
+    </>
+  )
+}
+
+export const query = graphql`
+  {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      limit: 4
+      sort: { fields: data___date, order: DESC }
+    ) {
+      nodes {
+        id
+        data {
+          date
+          name
+          type
+          image {
+            localFiles {
+              childImageSharp {
+                gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+              }
+            }
+          }
+        }
+      }
+    }
+
+    customers: allAirtable(filter: { table: { eq: "Customers" } }) {
+      nodes {
+        data {
+          name
+          quote
+          title
+          image {
+            localFiles {
+              childImageSharp {
+                gatsbyImageData(
+                  layout: FIXED
+                  width: 150
+                  height: 150
+                  placeholder: TRACED_SVG
+                )
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
+
+export default HomePage
